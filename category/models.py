c6f9d828fc11088b9587be3654002e3f2916e1b5
maheh <maheshmb7712@gmail.com>
2022-11-01 20:50:44 +0530

    created category model and registered in admin

--- category/models.py
+++ category/models.py
@@ -1,3 +1,20 @@
+from enum import unique
+from statistics import mode
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+    category_slug = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=100, blank=True)
+    cat_image = models.ImageField(upload_to='photos/category', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
