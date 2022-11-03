88a4231e1247fbd3fd193ac2afac511145d27d17
maheh <maheshmb7712@gmail.com>
2022-11-03 07:56:22 +0530

    configured password as readonly ,prepopulated fields,django media files

--- category/admin.py
+++ category/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Category
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'category_slug': ('category_name',)}
+    list_display=('category_name','category_slug')
 
-admin.site.register(Category)
+
+
+admin.site.register(Category,CategoryAdmin)
 
--- category/models.py
+++ category/models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100, unique=True)
-    category_slug = models.CharField(max_length=100, unique=True)
+    category_slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=100, blank=True)
     cat_image = models.ImageField(upload_to='photos/category', blank=True)
 
