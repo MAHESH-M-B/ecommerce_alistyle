7d4886d86f6275249cef0bda4d7f292a99d50ffd
maheh <maheshmb7712@gmail.com>
2022-11-03 19:24:29 +0530

    creating serializer for category

--- alistyle/settings.py
+++ alistyle/settings.py
@@ -123,7 +123,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'alistyle/static'
 ]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
--- category/views.py
+++ category/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import CategorySerializer
+from .models import Category
 # Create your views here.
+
+class CategoryViewset(viewsets.ModelViewSet):
+    serializer_class=CategorySerializer
+    queryset=Category.objects.all()
