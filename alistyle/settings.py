bd29c34dbfb08a95c8cbb2e2e204bf8cae1c0e9f
maheh <maheshmb7712@gmail.com>
2022-11-04 08:27:43 +0530

    created store app

--- alistyle/settings.py
+++ alistyle/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'category',
     'accounts',
-    'rest_framework',
+    'store',
 ]
 
 MIDDLEWARE = [
