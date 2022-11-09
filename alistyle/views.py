7fbea8d6b4f755fd96d206ec6445cd9aba36dc4d
maheh <maheshmb7712@gmail.com>
2022-11-09 21:59:26 +0530

    store

--- alistyle/views.py
+++ alistyle/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from store.models import Store
 def home(request):
     products=Store.objects.all().filter(is_available=True)
+    products_count = products.count()
     context={
-        'products':products
+        'products':products,
+        'products_count': products_count,
     }
     return render(request, 'index.html' ,context)
