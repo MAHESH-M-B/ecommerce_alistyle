39be5988a9aa004a71cd53cdee03dbdd5389f851
maheh <maheshmb7712@gmail.com>
2022-11-10 22:05:37 +0530

    urls

--- store/urls.py
+++ store/urls.py
@@ -2,10 +2,11 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .views import StoreViewset
-
+from alistyle import views
 router = routers.DefaultRouter()
 router.register(r'Category', StoreViewset)
 
 urlpatterns = [
     path('api/store_api/', include(router.urls)),
+    path('',views.home)
 ]
