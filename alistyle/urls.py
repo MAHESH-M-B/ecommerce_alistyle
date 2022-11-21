af7875bc87f3e1bb560e7129d9f0907d103a6d77
maheh <maheshmb7712@gmail.com>
2022-11-21 22:46:28 +0530

    nothing

--- alistyle/urls.py
+++ alistyle/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('<slug:category_slug>/',views.home,name='product_by_category_in_index'),
     path('', include('category.urls')),
     path('', include('accounts.urls')),
     path('', include('store.urls')),
--- alistyle/views.py
+++ alistyle/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from store.models import Store
-def home(request):
-    products=Store.objects.all().filter(is_available=True)
-    products_count = products.count()
-    context={
+from store import views
+from category.models import Category
+def home(request,category_slug=None):
+    categories=None
+    products=None
+    if category_slug !=None:
+        categories=get_object_or_404(Category,category_slug=category_slug)
+        products=Store.objects.filter(category=categories,is_available=True)
+        products_count=products.count()
+        context={
         'products':products,
         'products_count': products_count,
-    }
+         }
+    else:
+        products=Store.objects.all().filter(is_available=True)
+        products_count = products.count()
+        context={
+        'products':products,
+        'products_count': products_count,
+        }
+
     return render(request, 'index.html' ,context)
