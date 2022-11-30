76ea4c68da31dbf56f467f84938e22dbdaaa61fd
maheh <maheshmb7712@gmail.com>
2022-11-30 23:03:28 +0530

    added category links

--- category/models.py
+++ category/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -14,6 +14,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+    
+    def get_url(self):
+        return reverse('product_by_category',args=[self.category_slug])
 
     def __str__(self):
         return self.category_name
